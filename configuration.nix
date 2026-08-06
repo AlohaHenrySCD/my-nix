481a12fcca738c5bbabbf91a7e8573cfd811031f
AlohaHenry <aloha.henry.2018@gmail.com>
2026-08-06 16:13:44 +0800

    v0.0.3

--- configuration.nix
+++ configuration.nix
@@ -1,20 +1,144 @@
-{ inputs, pkgs, ... }:
+{ config, lib, inputs, pkgs, ... }:
 {
-  # users.groups.alohahenry = {};
-  # users.users.alohahenry.group = "alohahenry";
-  
-  # users.users.alohahenry.extraGroups = [ "wheel" ];
+  users.users.alohahenry = {
+    isNormalUser = true;
+    home = "/home/alohahenry";
+    extraGroups = [
+      "video"
+      "input"
+      "wheel"
+      "networkmanager"
+    ];
+    # shell = pkgs.fish;
+  };
+  nixpkgs.config.allowUnfree = true;
+
+  # brightness controll
+  hardware.brillo.enable = true;
+
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  # services.desktopManager.plasma6.enable = true;
   imports = [
     inputs.nixos-apple-silicon.nixosModules.default
     # Run `sudo nixos-generate-config --show-hardware-config | tee hardware-configuration.nix`
     # and uncomment this line.
     ./hardware-configuration.nix
   ];
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  services.logind.settings.Login = {
+    HandleLidSwitch = "suspend";
+    # HibernateDelaySec = "1h";
+    HandleLidSwitchExternalPower = "ignore";
+    # testing
+    HandleSuspendKey = "suspend";
+  };
+
+  # swapDevices = [
+  #   {
+  #     device = "/var/lib/swapfile";
+  #     size = 4*1024;
+  #   }
+  # ];
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [
+        "JetBrainsMono Nerd Font"
+        "Symbols Nerd Font Mono"
+      ];
+
+      sansSerif = [
+        "Noto Sans"
+        "Symbols Nerd Font"
+      ];
+
+      serif = [
+        "Noto Serif"
+        "Symbols Nerd Font"
+      ];
+    };
+  };
+
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        settings = {
+          main = {
+            capslock = "esc";
+            esc = "capslock";
+          };
+        };
+      };
+    };
+  };
+
+  fonts.packages = [pkgs.jetbrains-mono] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+
+  programs.niri.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        user = "alohahenry";
+        command = "${config.programs.niri.package}/bin/niri-session";
+      };
+    };
+  };
+  systemd.user.services.niri.enableDefaultPath = false;
+
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "eimadpbcbfnmbkopoojfekhnkhdbieeh" # dark reader
+      "hdokiejnpimakedhajhdlcegeplioahd" # lastpass
+      "dhdgffkkebhmkfjojejmpbldmpobfkfo" # tampermonkey
+      "bpoadfkcbjbfhfodiogcnhhhpibjhbnh" # immersive translate
+      "hfjbmagddngcpeloejdejnfgbamkjaeg" # vimium c
+      "bkdgflcldnnnapblkhphbgpggdiikppg" # duck duck go
+    ];
+    extraOpts = {
+      # "ExtensionSettings" = {
+      #   "bpoadfkcbjbfhfodiogcnhhhpibjhbnh" = {
+          
+      #   }
+      # };
+      "RestoreOnStartup" = 1;
+      "BackgroundModeEnabled" = false;
+    };
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+      fcitx5-fluent
+      (fcitx5-rime.override {
+        rimeDataPkgs = [
+          pkgs.rime-ice
+        ];
+      })
+    ];
+  };
+
+  programs.fish.enable = true;
+  users.extraUsers.alohahenry = {
+    shell = pkgs.fish;
+  };
 
   security.sudo = {
     enable = true;
+    wheelNeedsPassword = false;
     extraRules = [{
       commands = [
         {
@@ -27,10 +151,15 @@
   };
 
   boot = {
+    kernelParams = [
+      "appledrm.show_notch=1"
+    ];
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = false;
+    loader.systemd-boot.configurationLimit = 8;
   };
 
+  hardware.graphics.enable = true;
   hardware.asahi = {
     enable = true;
     peripheralFirmwareDirectory = /boot/vendorfw;
@@ -48,7 +177,13 @@
       "nix-command"
     ];
   };
+
+  time = {
+    timeZone = "Asia/Shanghai";
+  };
+  
   networking = {
+    # hostName = "alohahenry";
     networkmanager.enable = true;
     networkmanager.wifi.backend = "iwd";
     wireless.iwd = {
@@ -57,18 +192,87 @@
     };
   };
 
+  programs.starship.enable = true;
+
+  # services.mihomo = {
+  #   enable = true;
+  #   configFile = "/home/alohahenry/.config/mihomo.yaml";
+  # };
+
   environment.systemPackages = with pkgs; [
+    btop
+    lazygit
+    kdePackages.kate
+    papers
+    kitty
+    helix
+    starship
+    # go-musicfox
+    alacritty
+    fuzzel
+    home-manager
     chromium
     asahi-bless
     git
+    pulseaudio
+    tldr
+    navi
+    pavucontrol
+    fzf
+    i3bar-river
+    # notesnook
+    # obsidian
+    # anytype
+    # amplenote
+
+    # programing
+    rustc
+    cargo
+    rustfmt
+    clippy
+    rust-analyzer
+    # omniSharp
+    clang
+    clang-tools
+    bash-language-server
+    vscode-css-languageserver
+    yaml-language-server
+    fish-lsp
+    haskell-language-server
+    superhtml
+    typescript-language-server
+    vscode-json-languageserver
+    texlab
+    marksman
+    nil
+    ty
+    taplo
+    # ironbar
+    # upower
+    # fcft
+    # pactl
+    # nmcli
+    # blueman-manager
+    # peaclock
+    # playerctl
+    # swaync
   ];
 
-  users.mutableUsers = true;
+  environment.sessionVariables = lib.mkForce {
+    LC_MESSAGES = "zh-CN.UTF-8";
+    LANG = "zh_CN.UTF-8";
+    XMODIFIERS = "@im=fcitx";
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+    DISPLAY = "wayland-1";
+  };
 
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ];
-  # };
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  users.mutableUsers = true;
 
   system.stateVersion = "25.05";
 }
--- flake.nix
+++ flake.nix
@@ -3,6 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nirimod = {
+      url = "github:srinivasr/nirimod";
+    };
     nixos-apple-silicon = {
       url = "github:tpwrules/nixos-apple-silicon";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,16 +17,40 @@
   };
 
   outputs =
-    { nixpkgs, ... }@inputs:
+    { self, nixpkgs, nirimod, home-manager, ... }@inputs:
     let
       system = "aarch64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
+      # environment.systemPackages = [
+      #   inputs.nirimod.packages.${pkgs.system}.default
+      # ];
+      # devShells.${system}.default = pkgs.mkShell {
+      #   nativeBuildInputs = with pkgs; [
+      #     rustc
+      #     cargo
+      #     rustfmt
+      #     clippy
+      #     rust-analyzer
+      #   ];
+      #   RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+      # };
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs; };
-        modules = [ ./configuration.nix ];
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.default
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.alohahenry = import /home/alohahenry/.config/home-manager/home.nix;
+              # users.root = /home/alohahenry/.config/home-manager/home.nix;
+            };
+          }
+        ];
       };
 
       formatter.${system} = pkgs.nixfmt-rfc-style;
