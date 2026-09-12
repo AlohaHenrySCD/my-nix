e3cdd73329baf744605cf8c22651fd8e9d0376ec
AlohaHenry <aloha.henry.2018@gmail.com>
2026-09-12 11:26:02 +0800

    fd & update

--- home/modules/common/fish.nix
+++ home/modules/common/fish.nix
@@ -6,6 +6,7 @@
       set -g fish_greeting "Hello! AlohaHenry"
       set -g fish_cursor_default block
       abbr --add add --set-cursor='%' 'nix shell nixpkgs#%'
+      # abbr --add fd --set-cursor='%' 'find . -iname "*%*" 2>/dev/null'
     '';
     shellAbbrs = {
       ls = "eza";
@@ -27,9 +28,6 @@
       # '';
       manrg = ''
         man $argv[1] | col -b | rg -C 3 -- $argv[2]
-      '';
-      fd = ''
-        find . -iname $argv[1] 2>/dev/null
       '';
     };
   };
--- home/modules/common/packages.nix
+++ home/modules/common/packages.nix
@@ -24,6 +24,7 @@
     hugo
     lazygit
     btop
+    fd
 
     pkg-config
 
